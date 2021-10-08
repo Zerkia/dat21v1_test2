944564216db8573bbeee1333bcccedefc4e73947
Zerkia <lordzerkia@gmail.com>
2021-10-08 13:38:06 +0200

    first commit

---
+++ src/main/java/com/example/nep_dat21v1/NepDat21v1Application.java
@@ -0,0 +1,13 @@
+package com.example.nep_dat21v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepDat21v1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NepDat21v1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nep_dat21v1/NepDat21v1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nep_dat21v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepDat21v1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
